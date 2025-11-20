9aba649438b795d39c6bc22e219f1c1e462547fa
Ethan Jeff Mernilo <ethanjeffmernilo@gmail.com>
2025-11-21 02:15:46 +0800

    Completed front-end

--- src/Bigdiv.jsx
+++ src/Bigdiv.jsx
@@ -7,8 +7,34 @@ function Bigdiv(){
                 <img src="src/assets/provisionlogo.png" alt="" className="logo"/>
                 <img src="src/assets/bell.png" alt="" className="bell"/>
                 <img src="src/assets/home.png" alt="" className="home" />
+                <p className="hometext">Home</p>
+                <img src="src/assets/speechbubble.png" alt="" className="speechbubble" />
+                <p className="speechbubbletext">Messages</p>
+                <img src="src/assets/orders.png" alt="" className="orders" />
+                <p className="orderstext">Orders</p>
+                <div className="rectangle2"></div>
+                <img src="src/assets/statistics.png" alt="" className="statistics" />
+                <p className="statisticstext">Statistics</p>
+                <div className="line"></div>
+                <img src="src/assets/exit.png" alt="" className="exit" />
             </div>
-            <div className="headertext"></div>
+            <div className="header">
+                <div className="headertext">
+                    <h3 className="firstheadertext">
+                        Forecast
+                    </h3>
+                    <p className="secondheadertext">
+                        Company Name
+                    </p>
+                </div>
+                <div className="greenhome">
+                    <img src="src/assets/greenhome.png" alt="" className="greenhomepic" />
+                    <div className="backtodashboard">
+                        <p>Back To Dashboard</p>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     ); 
 }
